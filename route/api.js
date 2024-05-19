d4460469b310b55467adf285dffdf5f115db4715
Sudipta <engineer.sudipta.kumar.das@gmail.com>
2024-05-19 17:38:07 +0600

    Uploading Mobile Banking Management System Front-end (Full Version)

---
+++ route/api.js
@@ -0,0 +1,28 @@
+const API_ENDPOINTS = {
+    customerAuthIndex: "/api/customer/auth/index",
+    customerAuthSignup: "/api/customer/auth/signup",
+    customerAuthLogin: "/api/customer/auth/login",
+    customerAuthLogout: "/api/customer/auth/logout",
+    customerAuthChangePassword: "/api/customer/auth/change_password",
+    customerIndex: "/api/customer/index",
+    customerService: "/api/customer/customer_service",
+    customerSignupDetails: "/api/customer/signup/customer_details",
+    customerProfile: "/api/customer/profile",
+    customerProfileUpdate: "/api/customer/profile/update",
+    customerProfileUpload: "/api/customer/profile/upload",
+    customerViewProfileImage: "/api/customer/profile/view_profile_image",
+    customerSendMoney: "/api/customer/send_money",
+    customerCashOut: "/api/customer/cash_out",
+    customerBillPayment: "/api/customer/bill_payment",
+    customerCashIn: "/api/customer/cash_in",
+    customerWalletToBank: "/api/customer/add_money/wallet_to_bank",
+    customerWalletToCard: "/api/customer/add_money/wallet_to_card",
+    customerBankToWallet: "/api/customer/add_money/bank_to_wallet",
+    customerPaymentList: "/api/customer/payment/list",
+    customerPaymentCashInList: "/api/customer/payment/list/cash_in",
+    customerForgetPassword: "/api/customer/forget_password",
+    customerOTP: "/api/customer/otp",
+    navbarInfo: "/api/customer/navbar_info",
+};
+
+export default API_ENDPOINTS;
---
+++ route/routes.js
@@ -0,0 +1,21 @@
+const routes = {
+    root: '/',
+    login: '/user/Login',
+    signup: '/customer/Signup',
+    type: '/user/type',
+    customer_dashboard: '/customer/Dashboard',
+    forget_password: '/user/Forget_Password',
+    otp_verification: '/user/OTP',
+    new_password: '/user/New_Password',
+    create_customer_profile: '/customer/Info',
+    send_money: '/customer/Send_Money',
+    cash_out: '/customer/Cash_Out',
+    cash_in: '/customer/Cash_In',
+    wallet_to_bank: '/customer/Wallet_to_Bank',
+    bank_to_wallet: '/customer/Bank_to_Wallet',
+    payment_history: '/customer/Payment_History',
+    customer_profile: '/customer/My_Profile',
+
+};
+
+export default routes;
